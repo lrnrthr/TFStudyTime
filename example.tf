de46b4514f63af808d07254e75d1833f7dcedd33
Tracy P Holmes <12778804+tracypholmes@users.noreply.github.com>
2019-04-12 13:20:19 -0500

    Updated region to match UI

--- example.tf
+++ example.tf
@@ -1,7 +1,7 @@
 provider "aws" {
   access_key = "ACCESS_KEY_HERE"
   secret_key = "SECRET_KEY_HERE"
-  region     = "us-east-1"
+  region     = "us-east-2"
 }
 
 resource "aws_instance" "example" {
