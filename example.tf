3d97e36dbc995a2222c113d01d59257bb57d7e58
Lauren <lauren@hashicorp.com>
2019-04-24 13:25:03 -0400

    Update example.tf

--- example.tf
+++ example.tf
@@ -5,3 +5,5 @@ resource "aws_instance" "example" {
   ami           = "ami-02bcbb802e03574ba"
   instance_type = "t2.micro"
 }
+
+#one day my prince will come just like one day this policy will actually work.
